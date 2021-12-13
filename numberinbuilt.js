7a9ca68bee512cae4aeea866012cabb2d34cca28
Mounika <bojjamounika8@gmail.com>
2021-12-13 22:40:58 +0530

    numberinbuilt

---
+++ numberinbuilt.js
@@ -0,0 +1,30 @@
+// toString
+
+var num=50;
+console.log(typeof(num))
+console.log(num.toString())
+console.log(typeof(num.toString()))
+
+
+// parseInt
+
+var str='50';
+console.log(typeof(str))
+console.log(parseInt(str))
+console.log(typeof(parseInt(str)))
+
+
+//parsefloat
+
+var strNum = "15.276";
+console.log(strNum);
+console.log(typeof(strNum));
+console.log(parseFloat(strNum));
+
+
+//tofixed
+
+var num=11.23468759;
+console.log(num)
+console.log(num.toFixed(6))
+console.log(typeof(num))
